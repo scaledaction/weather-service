086f2fe3a23bd83826fb17570ed2d584ba544489
GitOutATown <aqumegeemail@gmail.com>
2015-11-01 17:03:39 -0800

    Connecting to Kafka producer.

--- ingest-api/src/main/scala/com/scaledaction/weatherservice/ingest/api/KafkaProducerActor.scala
+++ ingest-api/src/main/scala/com/scaledaction/weatherservice/ingest/api/KafkaProducerActor.scala
@@ -24,27 +24,31 @@ class KafkaProducerActor extends Actor with KafkaConfig {
   //  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
   //  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
   //
+  
+  // TODO: configuration instead of hardcoded...
+  val kafkaTopic = "killrweather.raw"
+  val kafkaBroker = "127.0.0.1:9092"
 
   val props = new Properties()
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaBrokers)
+  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBroker)
   props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafka.getString("key_serializer"))
   props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafka.getString("value_serializer"))
 
-  //val producer = new KafkaProducer[String, String](props)
+  val producer = new KafkaProducer[String, String](props)
+  println(s"producer: $producer")
 
   def receive: Receive = {
-    //case msg => println(s"producer received ${msg}")
-    //case tweet: Tweet =>
     case weatherData: RawWeatherData => 
-      //val record = new ProducerRecord[String,String](KafkaTopic, tweet.json)
-      val record = new ProducerRecord[String, String]("", "")
+      val record = new ProducerRecord[String,String](
+          kafkaTopic, weatherData.toJson.toString())
       println(s"KafkaTopic: ${KafkaTopic}, weatherData: ${weatherData.toJson}")
-    //          producer.send(record, new Callback {
-    //            override def onCompletion(result: RecordMetadata, exception: Exception) {
-    //              if (exception != null) {
-    //                log.warning("Failed to send record", exception)
-    //              }
-    //            }
-    //          })
+          producer.send(record, new Callback {
+              override def onCompletion(result: RecordMetadata, exception: Exception) {
+                  println(s"result: $result")
+                  if (exception != null) {
+                      log.warning("Failed to send record", exception)
+                  }
+              }
+          })
   }
 }
