24c1ed11276903b982c49923d488af70204fdf3d
jwbelew <jake@jakebelew.com>
2015-11-10 17:49:02 -0800

    added DataIngestBackendApp

--- ingest-backend/src/main/scala/com/scaledaction/weatherservice/ingest/backend/DataIngestBackendApp.scala
+++ ingest-backend/src/main/scala/com/scaledaction/weatherservice/ingest/backend/DataIngestBackendApp.scala
@@ -1,4 +1,4 @@
-package com.scaledaction.weatherservice.ingest.api
+package com.scaledaction.weatherservice.ingest.backend
 
 import com.scaledaction.core.cassandra.HasCassandraConfig
 import com.scaledaction.core.kafka.HasKafkaConfig
@@ -61,27 +61,4 @@ object DataIngestBackendApp extends App with HasCassandraConfig with HasKafkaCon
 
   ssc.start()
   ssc.awaitTermination()
-}
-
-//TODO - temp - use the JSON implicit val RawWeatherFormat = jsonFormat14(RawWeatherData) unmarshaller instead, 
-//cannot use RawWeatherData(_) 
-object RawWeatherDataFactory {
-  /** Tech debt - don't do it this way ;) */
-  def apply(array: Array[String]): RawWeatherData = {
-    RawWeatherData(
-      wsid = array(0),
-      year = array(1).toInt,
-      month = array(2).toInt,
-      day = array(3).toInt,
-      hour = array(4).toInt,
-      temperature = array(5).toDouble,
-      dewpoint = array(6).toDouble,
-      pressure = array(7).toDouble,
-      windDirection = array(8).toInt,
-      windSpeed = array(9).toDouble,
-      skyCondition = array(10).toInt,
-      skyConditionText = array(11),
-      oneHourPrecip = array(11).toDouble,
-      sixHourPrecip = Option(array(12).toDouble).getOrElse(0))
-  }
 }
